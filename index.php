eebc6e66ca83bec94e9c3232de38aa8e4d0a8b13
shkawan <shkawan@microsoft.com>
2016-06-09 01:53:02 +0900

    modify

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 
     <div class="sidebar pure-u-1 pure-u-md-1-4">
         <div class="header">
-            <h1 class="brand-title">A Sample Blog <?php echo date("Y/m/d h:i:s"); ?> </h1>
+            <h1 class="brand-title">A Sample Blog</h1>
             <h2 class="brand-tagline">Creating a blog layout using Pure</h2>
 
             <nav class="nav">
